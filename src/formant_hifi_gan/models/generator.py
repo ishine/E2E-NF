4b6e226f0cde4ee7a104e07a6916871e77266a40
misumisumi <dragon511southern@gmail.com>
2024-06-02 01:26:21 +0900

    feat: add formant_hifi_gan

--- src/formant_hifi_gan/models/generator.py
+++ src/formant_hifi_gan/models/generator.py
@@ -3,7 +3,12 @@ from logging import getLogger
 import torch
 import torch.nn as nn
 
-from formant_hifi_gan.layers import AdaptiveResidualBlock, Conv1d, ResidualBlock
+from formant_hifi_gan.layers import (
+    AdaptiveResidualBlock,
+    Conv1d,
+    ResidualBlock,
+    ResSkipBlock,
+)
 
 # A logger for this file
 logger = getLogger(__name__)
@@ -745,6 +750,270 @@ class SiFiGANDirectGenerator(nn.Module):
         c = self.fn["output_conv"](c)
 
         return c, e
+
+    def reset_parameters(self):
+        """Reset parameters.
+
+        This initialization follows the official implementation manner.
+        https://github.com/jik876/hifi-gan/blob/master/models.py
+
+        """
+
+        def _reset_parameters(m):
+            if isinstance(m, (nn.Conv1d, nn.ConvTranspose1d)):
+                m.weight.data.normal_(0.0, 0.01)
+                logger.debug(f"Reset parameters in {m}.")
+
+        self.apply(_reset_parameters)
+
+    def remove_weight_norm(self):
+        """Remove weight normalization module from all of the layers."""
+
+        def _remove_weight_norm(m):
+            try:
+                logger.debug(f"Weight norm is removed from {m}.")
+                nn.utils.remove_weight_norm(m)
+            except ValueError:  # this module didn't have weight norm
+                return
+
+        self.apply(_remove_weight_norm)
+
+    def apply_weight_norm(self):
+        """Apply weight normalization module from all of the layers."""
+
+        def _apply_weight_norm(m):
+            if isinstance(m, nn.Conv1d) or isinstance(m, nn.ConvTranspose1d):
+                nn.utils.parametrizations.weight_norm(m)
+                logger.debug(f"Weight norm is applied to {m}.")
+
+        self.apply(_apply_weight_norm)
+
+
+class FormantHiFiGANGenerator(nn.Module):
+    """HiFiGAN generator module with customizable pitch-dependent mechanisms."""
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels=1,
+        channels=512,
+        kernel_size=7,
+        upsample_scales=(5, 4, 3, 2),
+        upsample_kernel_sizes=(10, 8, 6, 4),
+        qp_resblock_kernel_size=3,
+        qp_resblock_dilations=[(1), (1, 2), (1, 2, 4), (1, 2, 4, 8)],
+        qp_use_additional_convs=True,
+        resblock_kernel_sizes=(3, 7, 11),
+        resblock_dilations=[(1, 3, 5), (1, 3, 5), (1, 3, 5)],
+        resskipblock_dilations=[(1, 2, 4), (1, 2, 4)],
+        resskipblock_channels=1024,
+        resskipblock_gate_channels=128,
+        resskipblock_skip_out_channels=1024,
+        resskipblock_kernel_size=3,
+        use_additional_convs=False,
+        use_sine_embs=False,
+        use_qp_resblocks=False,
+        bias=True,
+        nonlinear_activation="LeakyReLU",
+        nonlinear_activation_params={"negative_slope": 0.1},
+        use_weight_norm=True,
+    ):
+        """Initialize QpHiFiGANGenerator module.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            channels (int): Number of hidden representation channels.
+            kernel_size (int): Kernel size of initial and final conv layer.
+            upsample_scales (list): List of upsampling scales.
+            upsample_kernel_sizes (list): List of kernel sizes for upsampling layers.
+            qp_resblock_kernel_size (int): Kernel size for quasi-periodic residual blocks.
+            qp_resblock_dilations (list): Dilations for quasi-periodic residual blocks.
+            qp_use_additional_convs (bool): Whether to use additional conv layers.
+            resblock_kernel_sizes (list): List of kernel sizes for residual blocks.
+            resblock_dilations (list): List of dilation list for residual blocks.
+            resskipblock_layers (int): Number of layers of resskipblock
+            resskipblock_stacks (int): Number of conv in resskipblock layers
+            resskipblock_gate_channels(int): Channels of gate
+            resskipblock_kernel_size(int): Kernel size of conv in resskipblock
+            use_additional_convs (bool): Whether to use additional conv layers in residual blocks.
+            use_sine_embs (bool): Whether to use sine embeddings.
+            use_qp_resblocks (bool): Whether to use quasi-periodic residual blocks.
+            bias (bool): Whether to add bias parameter in convolution layers.
+            nonlinear_activation (str): Activation function module name.
+            nonlinear_activation_params (dict): Hyperparameters for activation function.
+            use_weight_norm (bool): Whether to use weight norm.
+                If set to true, it will be applied to all of the conv layers.
+
+        """
+        super().__init__()
+        # check hyperparameters are valid
+        assert kernel_size % 2 == 1, "Kernel size must be odd number."
+        assert len(upsample_scales) == len(upsample_kernel_sizes)
+        assert len(resblock_dilations) == len(resblock_kernel_sizes)
+
+        # define modules
+        self.input_conv = Conv1d(
+            in_channels,
+            resskipblock_channels,
+            kernel_size,
+            bias=bias,
+            padding=(kernel_size - 1) // 2,
+        )
+        self.resskipblocks = nn.ModuleList()
+        for dilations in resskipblock_dilations:
+            for dilation in dilations:
+                conv = ResSkipBlock(
+                    resskipblock_channels,
+                    resskipblock_gate_channels,
+                    resskipblock_kernel_size,
+                    resskipblock_skip_out_channels,
+                    dilation=dilation,
+                )
+                self.resskipblocks.append(conv)
+        self.post_resskip = nn.ModuleList()
+        if resskipblock_skip_out_channels != channels:
+            self.post_resskip = self.post_resskip.extend(
+                [
+                    nn.ReLU(),
+                    nn.Conv1d(resskipblock_skip_out_channels, channels, kernel_size=1, stride=1, bias=True),
+                ]
+            )
+
+        self.num_upsamples = len(upsample_kernel_sizes)
+        self.qp_resdual_dilations = qp_resblock_dilations
+        self.num_blocks = len(resblock_kernel_sizes)
+        self.upsamples = nn.ModuleList()
+        self.use_qp_resblocks = use_qp_resblocks
+        if use_qp_resblocks:
+            self.qp_blocks = nn.ModuleList()
+        self.blocks = nn.ModuleList()
+        for i in range(len(upsample_kernel_sizes)):
+            assert upsample_kernel_sizes[i] == 2 * upsample_scales[i]
+            self.upsamples += [
+                nn.Sequential(
+                    getattr(nn, nonlinear_activation)(**nonlinear_activation_params),
+                    nn.ConvTranspose1d(
+                        channels // (2**i),
+                        channels // (2 ** (i + 1)),
+                        upsample_kernel_sizes[i],
+                        upsample_scales[i],
+                        padding=upsample_scales[i] // 2 + upsample_scales[i] % 2,
+                        output_padding=upsample_scales[i] % 2,
+                        bias=bias,
+                    ),
+                )
+            ]
+            if use_qp_resblocks:
+                self.qp_blocks += [
+                    AdaptiveResidualBlock(
+                        kernel_size=qp_resblock_kernel_size,
+                        channels=channels // (2 ** (i + 1)),
+                        dilations=qp_resblock_dilations[i],
+                        bias=bias,
+                        use_additional_convs=qp_use_additional_convs,
+                        nonlinear_activation=nonlinear_activation,
+                        nonlinear_activation_params=nonlinear_activation_params,
+                    )
+                ]
+            for j in range(len(resblock_kernel_sizes)):
+                self.blocks += [
+                    ResidualBlock(
+                        kernel_size=resblock_kernel_sizes[j],
+                        channels=channels // (2 ** (i + 1)),
+                        dilations=resblock_dilations[j],
+                        bias=bias,
+                        use_additional_convs=use_additional_convs,
+                        nonlinear_activation=nonlinear_activation,
+                        nonlinear_activation_params=nonlinear_activation_params,
+                    )
+                ]
+        self.output_conv = nn.Sequential(
+            nn.LeakyReLU(),
+            nn.Conv1d(
+                channels // (2 ** (i + 1)),
+                out_channels,
+                kernel_size,
+                bias=bias,
+                padding=(kernel_size - 1) // 2,
+            ),
+            nn.Tanh(),
+        )
+
+        # sine embedding layers
+        self.use_sine_embs = use_sine_embs
+        if use_sine_embs:
+            self.emb = Conv1d(
+                1,
+                channels // (2 ** len(upsample_kernel_sizes)),
+                kernel_size,
+                bias=bias,
+                padding=(kernel_size - 1) // 2,
+            )
+            self.downsamples = nn.ModuleList()
+            for i in reversed(range(len(upsample_kernel_sizes))):
+                self.downsamples += [
+                    nn.Sequential(
+                        nn.Conv1d(
+                            channels // (2 ** (i + 1)),
+                            channels // (2**i),
+                            upsample_kernel_sizes[i],
+                            upsample_scales[i],
+                            padding=upsample_scales[i] - (upsample_kernel_sizes[i] % 2 == 0),
+                            bias=bias,
+                        ),
+                        getattr(nn, nonlinear_activation)(**nonlinear_activation_params),
+                    )
+                ]
+
+        # apply weight norm
+        if use_weight_norm:
+            self.apply_weight_norm()
+
+        # reset parameters
+        self.reset_parameters()
+
+    def forward(self, x, c, d=None):
+        """Calculate forward propagation.
+
+        Args:
+            x (Tensor): Input sine signal (B, 1, T).
+            c (Tensor): Input tensor (B, in_channels, T).
+            d (List): F0-dependent dilation factors [(B, 1, T) x num_upsamples].
+
+        Returns:
+            Tensor: Output tensor (B, out_channels, T).
+
+        """
+        c = self.input_conv(c)
+
+        if self.use_sine_embs:
+            x = self.emb(x)
+            embs = [x]
+            for i in range(self.num_upsamples - 1):
+                x = self.downsamples[i](x)
+                embs += [x]
+        skips = 0
+        for f in self.resskipblocks:
+            c, h = f(c)
+            skips += h
+        c = skips
+        for f in self.post_resskip:
+            c = f(c)
+
+        for i in range(self.num_upsamples):
+            c = self.upsamples[i](c)
+            if self.use_sine_embs:
+                c = c + embs[-i - 1]
+            if self.use_qp_resblocks:
+                c = self.qp_blocks[i](c, d[i])
+            cs = 0.0  # initialize
+            for j in range(self.num_blocks):
+                cs += self.blocks[i * self.num_blocks + j](c)
+            c = cs / self.num_blocks
+        c = self.output_conv(c)
+
+        return (c,)
 
     def reset_parameters(self):
         """Reset parameters.
